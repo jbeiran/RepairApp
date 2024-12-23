b451537cc93f40780e6c78ed20d113e625e268ab
jbeiran <josemibeiran@gmail.com>
2024-12-23 19:58:11 +0000

    feat: enum type class

---
+++ src/org/ulpgc/is1/model/DeviceType.java
@@ -0,0 +1,8 @@
+package org.ulpgc.is1.model;
+
+public enum DeviceType {
+    Desktop,
+    Laptop,
+    Smartphone,
+    Tablet
+}
---
+++ src/org/ulpgc/is1/model/ServiceType.java
@@ -0,0 +1,7 @@
+package org.ulpgc.is1.model;
+
+public enum ServiceType {
+    Assembly,
+    Maintenance,
+    Repair
+}
