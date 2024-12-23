093fb8d58ea59c9734171887cb90e1239af0eb65
jbeiran <josemibeiran@gmail.com>
2024-12-23 20:07:46 +0000

    feat: create Work class

---
+++ src/org/ulpgc/is1/model/Work.java
@@ -0,0 +1,26 @@
+package org.ulpgc.is1.model;
+
+public class Work {
+    private final int timeSpent;
+    private final String description;
+    private final Employee technician;
+
+    public Work(int timeSpent, String description, Employee technician) {
+        this.timeSpent = timeSpent;
+        this.description = description;
+        this.technician = technician;
+        technician.addWork(this);
+    }
+
+    public int getTimeSpent() {
+        return timeSpent;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Employee getTechnician() {
+        return technician;
+    }
+}
