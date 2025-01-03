6b95e98916fa1d63bdab797c7d21e024b0b7a393
jbeiran <josemibeiran@gmail.com>
2025-01-03 01:16:12 +0000

    refactor: enhance Phone class validation by adding number format check in constructor

--- src/org/ulpgc/is1/model/Phone.java
+++ src/org/ulpgc/is1/model/Phone.java
@@ -4,17 +4,20 @@ public class Phone {
     private final String number;
 
     public Phone(String number) {
+        if (!isValid(number)) {
+            throw new IllegalArgumentException("El número de teléfono no es válido");
+        }
         this.number = number;
     }
 
-    public boolean isValid() {
+    private boolean isValid(String number) {
         return number != null && number.matches("\\d{9}");
     }
 
     public String getNumber() {
         return number;
     }
-
+        
     @Override
     public String toString() {
         return number;
