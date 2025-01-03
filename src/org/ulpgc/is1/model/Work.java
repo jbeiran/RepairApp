cd7d456149dceabafcc431156d50f01433e9f548
jbeiran <josemibeiran@gmail.com>
2025-01-03 01:13:01 +0000

    feat: add technician field to Work class and corresponding getter method

--- src/org/ulpgc/is1/model/Work.java
+++ src/org/ulpgc/is1/model/Work.java
@@ -3,10 +3,12 @@ package org.ulpgc.is1.model;
 public class Work {
     private final int timeSpent;
     private final String description;
+    private final Employee technician;
 
-    public Work(int timeSpent, String description) {
+    public Work(int timeSpent, String description, Employee technician) {
         this.timeSpent = timeSpent;
         this.description = description;
+        this.technician = technician;
     }
 
     public int getTimeSpent() {
@@ -15,5 +17,9 @@ public class Work {
 
     public String getDescription() {
         return description;
+    }
+    
+    public Employee getTechnician() {
+        return technician;
     }
 }
